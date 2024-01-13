77741dfd09ace84d9b54a1c9c9715f8db5eff430
Dextermxck111 <120288352+Dextermxck111@users.noreply.github.com>
2024-01-13 13:20:51 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Box from "./components/box";
+
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav>
+
+      <ul>
+        <li>About Us</li>
+
+        <li>
+          <img></img>
+        </li>
+
+        <li>Donate</li>
+
+      </ul>
+
+    </nav>
   );
 }
 
