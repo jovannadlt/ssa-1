cc0af25ecd43d76317e3a5c4ee7c12ad367aed1a
Jovanna <jovannadelatrinidad@gmail.com>
2024-01-13 14:59:13 -0800

    fixing homepage and navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
+import About from './webpages/About'; // Import the About component
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home'); // Initial page is 'home'
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <nav>
+    <div>
+      <nav>
+        <ul>
+          <li onClick={() => handlePageChange('home')}>Home</li>
+          <li onClick={() => handlePageChange('about')}>About Us</li>
+          <li onClick={() => handlePageChange('donate')}>Donate</li>
+        </ul>
+      </nav>
 
-      <ul>
-        <li>About Us</li>
-
-        <li>
-          <img></img>
-        </li>
-
-        <li>Donate</li>
-
-      </ul>
-
-    </nav>
+      {/* Render different components based on the current page */}
+      {currentPage === 'home' && <div>Home Page Content</div>}
+      {currentPage === 'about' && <About />} {/* Render the About component */}
+      {currentPage === 'donate' && <div>Donate Page Content</div>}
+    </div>
   );
 }
 
